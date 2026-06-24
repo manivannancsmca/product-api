522a6742cce07fb09a14c94c8ba81d25b57c4169
manivannancsmca <manivannancsmca@gmail.com>
2026-06-24 23:22:13 +0530

    Remove Javadoc comment from DuplicateResourceException
    Removed Javadoc comment for DuplicateResourceException.

--- src/main/java/com/example/productmanagement/exception/DuplicateResourceException.java
+++ src/main/java/com/example/productmanagement/exception/DuplicateResourceException.java
@@ -1,8 +1,6 @@
 package com.example.productmanagement.exception;
 
-/**
- * Exception thrown when a resource with duplicate unique identifier already exists.
- */
+
 public class DuplicateResourceException extends RuntimeException {
 
     public DuplicateResourceException(String message) {
